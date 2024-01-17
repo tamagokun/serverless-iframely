1343d7ab6384d4dae25bab809b16260ed7d5cbbf
Mike Kruk <mike@ripeworks.com>
2024-01-17 13:41:02 -0500

    more debug

--- plugins/domains/h5p.com/h5p.com.js
+++ plugins/domains/h5p.com/h5p.com.js
@@ -2,7 +2,23 @@ export default {
   re: [/https?:\/\/[a-zA-Z0-9]*\.?h5p\.com/i],
   mixins: ["*"],
 
+  getLink(url, meta) {
+    console.log("> plugin called getLink");
+    console.log(JSON.stringify({ url, meta }, null, 2));
+    return {
+      template_context: {
+        iframe_src: url,
+        title: meta["html-title"],
+        height: 400,
+      },
+      rel: [CONFIG.R.html5],
+      type: CONFIG.T.text_html,
+    };
+  },
+
   getLinks(url, meta) {
+    console.log("> plugin called getLinks");
+    console.log(JSON.stringify({ url, meta }, null, 2));
     return {
       template_context: {
         iframe_src: url,
